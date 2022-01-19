5b8a476f22d0ecde53e32fc155da8d6095d80d95
Maximilian <hitch2290@gmail.com>
2022-01-19 19:21:53 +0300

    Task2 homework updated

--- Task2/src/main/java/Task2.java
+++ Task2/src/main/java/Task2.java
@@ -15,13 +15,13 @@ public class Task2 {
         String str = "My name Maximilian";
         System.out.println("What is your name?" + "\n" + str + "\n");
         char chLit = 'M';
-        System.out.println((byte)chLit + "\n");
+        System.out.println(chLit + "\n");
         int someNumInt = 156;
         System.out.println("in Binary system " + Integer.toBinaryString(someNumInt) + "\n");
         System.out.println("in Octal system " + Integer.toOctalString(someNumInt) + "\n");
         System.out.println("in Normal system " + someNumInt + "\n");
         System.out.println("in Hex system " + Integer.toHexString(someNumInt) + "\n");
-        System.out.println((float) Math.random() + "\n");
+        System.out.println(1.46568 + "\n");
         System.out.println(Math.random() + "\n");
 //        вывести на консоль литералы следующих видов:
 //        логический
@@ -41,9 +41,12 @@ public class Task2 {
      **/
     public static int sum(int a, int b) {
         int sum = a + b;
-        if (sum > Integer.MAX_VALUE)
+        if (sum > Integer.MAX_VALUE || a >= Integer.MAX_VALUE || b >= Integer.MAX_VALUE) {
             return -1;
-        else return sum;
+        }
+        else {
+            return sum;
+        }
     }
 
     /**
@@ -79,7 +82,7 @@ public class Task2 {
      * return 20
      */
     public static double calculateHypotenuse(int a, int b) {
-        int hipotenuza = a * a + b * b;
-        return Math.round(Math.sqrt(hipotenuza));
+        int hypotenuse = a * a + b * b;
+        return Math.round(Math.sqrt(hypotenuse));
     }
 }

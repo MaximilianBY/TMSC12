2e42f4da4a79fb468ca404f03ea2c3dfa78e5f23
Maximilian <hitch2290@gmail.com>
2022-07-18 11:11:20 +0300

    Hibernate HW

---
+++ Eshop/src/main/java/by/tms/eshop/EshopConstants.java
@@ -0,0 +1,9 @@
+package by.tms.eshop;
+
+public interface EshopConstants {
+
+  String USER = "user";
+  String SHOPPING_CART = "cart";
+  String PRODUCT_ID = "product_id";
+  String CATEGORY_ID = "category_id";
+}
---
+++ Eshop/src/main/java/by/tms/eshop/repositories/CategoryDao.java
@@ -0,0 +1,9 @@
+package by.tms.eshop.repositories;
+
+import by.tms.eshop.entities.Category;
+import java.util.Set;
+
+public interface CategoryDao {
+
+  Set<Category> getAllCategories();
+}
